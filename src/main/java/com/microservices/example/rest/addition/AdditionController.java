bd19b65873a4fb4cfa2c77b7e367e9dae22a8ae8
ganesh_pankajakshan <ganesh_pankajakshan@LnDSASVA-11>
2025-02-18 13:39:51 +0530

    Please create a PR

--- src/main/java/com/microservices/example/rest/addition/AdditionController.java
+++ src/main/java/com/microservices/example/rest/addition/AdditionController.java
@@ -1,26 +1,42 @@
 package com.microservices.example.rest.addition;
 
 import java.util.logging.Logger;
-
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-//REST API Controller class 
+
+// REST API Controller class 
 @RestController
 public class AdditionController {
 
-	protected Logger logger = Logger.getLogger(AdditionController.class
-			.getName());
+    protected Logger logger = Logger.getLogger(AdditionController.class.getName());
 
-	//The request add will be mapped to a method doAdd 
-	@RequestMapping("/add")
-	public String doAdd(@RequestParam(defaultValue="0") String addend1,
-						@RequestParam(defaultValue="0") String addend2) {
+    // The request add will be mapped to a method doAdd 
+    @GetMapping("/add")
+    public String doAdd(@RequestParam(value = "addend1", defaultValue = "0") String addend1,
+                        @RequestParam(value = "addend2", defaultValue = "0") String addend2) {
 
-		int augend1 = Integer.valueOf(addend1);
-		int augend2 = Integer.valueOf(addend2);
-		int sum = augend1 * augend2;
-		logger.info("Addition is "+sum);
-		return "{\"addend1\":\"" + addend1 + "\", \"addend2\":\"" + addend2 + "\", \"sum\": \"" + sum + "\"}";
-	}
+        int num1;
+        int num2;
+        try {
+            num1 = Integer.parseInt(addend1);
+        } catch (NumberFormatException e) {
+            logger.warning("Invalid input for addend1: " + addend1);
+            return "Invalid input for addend1. Please provide a valid integer.";
+        }
+        try {
+            num2 = Integer.parseInt(addend2);
+        } catch (NumberFormatException e) {
+            logger.warning("Invalid input for addend2: " + addend2);
+            return "Invalid input for addend2. Please provide a valid integer.";
+        }
+
+        int sum = calculate(num1, num2);
+        logger.info("Addition is " + sum);
+        return String.format("{\"addend1\": %d, \"addend2\": %d, \"sum\": %d}", num1, num2, sum);
+    }
+
+    private int calculate(int a, int b) {
+        return a + b;
+    }
 }
